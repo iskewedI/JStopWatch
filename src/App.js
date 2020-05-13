5a75be2e483c63bc2517ab5f8f0ce9ab3d883b33
iskewedi <jtornello2019@hotmail.com>
2020-05-13 15:56:54 -0300

    Initial functions, stopwatch and countdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './styles/App.css';
+import Stopwatch from './components/Stopwatch';
+import Countdown from './components/Countdown';
+import Toggle from './components/Toggle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {stopwatchIcon, countdownIcon} from './components/Icons.js';
+
+class App extends Component {
+  state = {
+    isStopwatch:false,
+  }
+  changeTimer = () => {
+    this.setState({
+      isStopwatch: !this.state.isStopwatch
+    })
+  };
+  render(){
+    return (
+      <div className="App">
+        <Toggle onChange={this.changeTimer} icons={[stopwatchIcon, countdownIcon]} className="changeTimer"/>
+        {this.state.isStopwatch && (
+          <Stopwatch />
+        )}
+        {!this.state.isStopwatch && (
+          <Countdown />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
