cc22424686f719a0817bdc4e94606b1cbd312505
iskewedi <jtornello2019@hotmail.com>
2020-05-18 10:54:34 -0300

    Updates

---
+++ precache-manifest.b7d8feab2c7d2e2ee5e4957283c71248.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70300b918eaef1e12a3636ebc8044ceb",
+    "url": "/jstopwatch/index.html"
+  },
+  {
+    "revision": "3b7c7346f45f3334e4ee",
+    "url": "/jstopwatch/static/css/2.83e566b4.chunk.css"
+  },
+  {
+    "revision": "3a5e0ffa02b37aa4bef7",
+    "url": "/jstopwatch/static/css/main.6cecaf8b.chunk.css"
+  },
+  {
+    "revision": "3b7c7346f45f3334e4ee",
+    "url": "/jstopwatch/static/js/2.f6092dc9.chunk.js"
+  },
+  {
+    "revision": "08270ead6a17e8cb4839d8bf0867c734",
+    "url": "/jstopwatch/static/js/2.f6092dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5e0ffa02b37aa4bef7",
+    "url": "/jstopwatch/static/js/main.d5e175ad.chunk.js"
+  },
+  {
+    "revision": "2e03f0d6ed97db238076",
+    "url": "/jstopwatch/static/js/runtime-main.7ebbff02.js"
+  },
+  {
+    "revision": "8b0201a8d84c53ee445805d1ec89ebcb",
+    "url": "/jstopwatch/static/media/twoTick-clock-sound.8b0201a8.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jstopwatch/precache-manifest.c318c12c0b1fca48ded749934afb6915.js"
+  "/jstopwatch/precache-manifest.b7d8feab2c7d2e2ee5e4957283c71248.js"
 );
 
 self.addEventListener('message', (event) => {
